a417779f73c002f6a0b0b8347c5a6b201e468411
Petro Silenius <petro.silenius@gmail.com>
2022-11-05 17:23:42 +0200

    Remove register pages and move onboard directly to /onboard

--- app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="space-y-6">
-      <div className="space-y-8 text-white">
-        <h1>Login</h1>
-      </div>
-    </div>
-  );
-}
