5967e8675153e50364aab15c45d649a1f1635caa
Petro Silenius <petro.silenius@gmail.com>
2022-11-05 06:14:41 +0200

    Create new answer on form submit and redirect to next question

--- pages/api/wizard/[questionType].ts
+++ pages/api/wizard/[questionType].ts
@@ -1,18 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+enum QuestionTypeOrder {
+  ACTIVITY = 1,
+  POSITIVE_TRAIT = 2,
+  NEGATIVE_TRAIT = 3,
+  result = 4,
+}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const questionType = req.query.questionType;
   const body = req.body;
-
-  const chosenOptions = Object.keys(body);
-
-  console.log({ chosenOptions });
 
   // Properly validate the data
   if (!body || !questionType || questionType instanceof Array) {
     return res.status(400).json({ data: 'Missing body' });
   }
 
-  // Write to database
-  res.redirect(302, `/app/wizard/${questionType}`);
+  const chosenOptions = Object.keys(body);
+
+  prisma.answers.create({
+    data: {
+      question_id: body.question_id,
+      value: chosenOptions,
+      user_id: '123',
+    },
+  });
+
+  const nextQuestionType =
+    QuestionTypeOrder[
+      QuestionTypeOrder[questionType as keyof typeof QuestionTypeOrder] + 1
+    ];
+
+  res.redirect(302, `/app/wizard/${nextQuestionType}`);
 }
