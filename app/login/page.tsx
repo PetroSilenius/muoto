bfc44061b6cb492905e7c5742ee176a751725ba6
Petro Silenius <petro.silenius@gmail.com>
2022-11-05 21:56:18 +0200

    Test if serve component redirection causes client-side exception in vercel

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,13 @@
-import { getSession } from '@/lib/session';
-import { headers } from 'next/headers';
+// import { getSession } from '@/lib/session';
+// import { headers } from 'next/headers';
 import LogInButton from './LogInButton';
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  const session = await getSession(headers().get('cookie'));
-  if (session) {
-    redirect('/app/dashboard');
-  }
+  // const session = await getSession(headers().get('cookie'));
+  // if (session) {
+  //   redirect('/app/dashboard');
+  // }
 
   return (
     <div className="flex flex-col items-center space-y-8 text-center text-white">
