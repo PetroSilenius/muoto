39b4d422c137438983e64f886f4bee19c7c87903
Petro Silenius <petro.silenius@gmail.com>
2022-11-05 18:52:46 +0200

    Redirect to survey on logIn

--- app/login/LogInButton.tsx
+++ app/login/LogInButton.tsx
@@ -5,7 +5,7 @@ import { signIn } from 'next-auth/react';
 export default function LogInButton() {
   return (
     <button
-      onClick={() => signIn('google')}
+      onClick={() => signIn('google', { callbackUrl: '/app/steps/activity' })}
       className="w-fit rounded-md bg-muoto-red p-2"
     >
       Sign in with Google
