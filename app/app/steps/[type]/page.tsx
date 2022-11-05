c37edc30e095e644e52a7e12dfde714815927574
Petro Silenius <petro.silenius@gmail.com>
2022-11-06 00:03:58 +0200

    Comment out data fetching for debuggin again

--- app/app/steps/[type]/page.tsx
+++ app/app/steps/[type]/page.tsx
@@ -1,19 +1,19 @@
 import Tag from 'app/app/steps/Tag';
-import type { questionType } from '@prisma/client';
-import client from 'lib/prismadb';
+// import type { questionType } from '@prisma/client';
+// import client from 'lib/prismadb';
 
-async function fetchData(params: { type: questionType }) {
-  const questionType = params.type;
+// async function fetchData(params: { type: questionType }) {
+//   const questionType = params.type;
 
-  return await client.questions.findFirstOrThrow({
-    where: {
-      type: questionType,
-    },
-    include: {
-      options: true,
-    },
-  });
-}
+//   return await client.questions.findFirstOrThrow({
+//     where: {
+//       type: questionType,
+//     },
+//     include: {
+//       options: true,
+//     },
+//   });
+// }
 
 export default async function Page({
   params,
@@ -21,18 +21,18 @@ export default async function Page({
   params: { type: any };
   children?: React.ReactNode;
 }) {
-  const data = await fetchData(params);
+  const data = {} as any; //  await fetchData(params);
 
   return (
-    <form method="POST" action={`/api/steps/${data.type}`}>
+    <form method="POST" action={`/api/steps/${data?.type}`}>
       <div className="space-y-4 px-4">
         <h1 className="text-shadow text-4xl font-medium text-muoto-orange">
-          {data.content}
+          {data?.content}
         </h1>
         <p className="font-medium text-zinc-500">{data.description}</p>
         <div className="flex flex-row flex-wrap gap-4 ">
           <input type="hidden" name="questionId" value={data.id} />
-          {data.options.map((option) => (
+          {data.options.map((option: any) => (
             <div key={option.id}>
               <Tag option={option} />
             </div>
