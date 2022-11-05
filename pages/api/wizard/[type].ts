f19cd18442dd5c9bdb2027f40dffad361a155482
Petro Silenius <petro.silenius@gmail.com>
2022-11-05 16:23:56 +0200

    Use real user id when creating answers

--- pages/api/wizard/[type].ts
+++ pages/api/wizard/[type].ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import client from 'lib/prismadb';
+import { unstable_getServerSession } from 'next-auth/next';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
 
 enum QuestionTypeOrder {
   activity,
@@ -8,7 +10,10 @@ enum QuestionTypeOrder {
   result,
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const questionType = req.query.type;
   const body = req.body;
 
@@ -17,13 +22,18 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(400).json({ data: 'Missing body' });
   }
 
+  const session = await unstable_getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ data: 'Unauthorized' });
+  }
+
   const chosenOptions = Object.keys(body);
 
   client.answers.create({
     data: {
       question_id: body.question_id,
       value: chosenOptions,
-      user_id: '123',
+      user_id: session.user.id,
     },
   });
 
