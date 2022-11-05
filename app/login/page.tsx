f9177ac216346e1eae5a928e7615c06f4b832519
Petro Silenius <petro.silenius@gmail.com>
2022-11-05 18:53:24 +0200

    If valid session when navigating to /login, redirect to dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,14 @@
+import { getSession } from '@/lib/session';
+import { headers } from 'next/headers';
 import LogInButton from './LogInButton';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
+  const session = await getSession(headers().get('cookie'));
+  if (session) {
+    redirect('/app/dashboard');
+  }
+
   return (
     <div className="flex flex-col items-center space-y-8 text-center text-white">
       <LogInButton />
