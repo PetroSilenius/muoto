61e3380ac42b5b0be10ed00954ae29b3587987a9
Petro Silenius <petro.silenius@gmail.com>
2022-11-05 02:33:15 +0200

    Remove black background from the page content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import '@/styles/globals.css';
-import AddressBar from '@/ui/AddressBar';
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <title>Next.js App Directory Playground</title>
       </head>
       <body className="overflow-y-scroll bg-muoto-blue">
-        <div className="grid grid-cols-[min(800px,100%)] place-content-center py-8">
-          <div className="rounded-xl border border-zinc-800 bg-black p-8">
-            {children}
-          </div>
+        <div className="grid min-h-screen grid-cols-[min(800px,100%)] place-content-center py-8	">
+          {children}
         </div>
       </body>
     </html>
