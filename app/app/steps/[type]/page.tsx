f7daaa2e1ac768b74e62780a3140a0202569768c
Dusan Turajlic <dusan@turajlic.fi>
2022-11-05 23:44:40 +0200

    removes debug code

--- app/app/steps/[type]/page.tsx
+++ app/app/steps/[type]/page.tsx
@@ -1,25 +1,19 @@
 import Tag from 'app/app/steps/Tag';
-// import { questionType } from '@prisma/client';
-// import client from 'lib/prismadb';
+import { questionType } from '@prisma/client';
+import client from 'lib/prismadb';
 
-// async function fetchData(params: { type: questionType }) {
-//   const questionType = params.type;
+async function fetchData(params: { type: questionType }) {
+  const questionType = params.type;
 
-//   try {
-//       return await client.questions
-//           .findFirstOrThrow({
-//               where: {
-//                   type: questionType,
-//               },
-//               include: {
-//                   options: true,
-//               },
-//           })
-//   } catch (e) {
-//       console.error(e)
-//       return {id: 'Error', type: 'Error', description: 'Error', content: 'Error', options: []};
-//   }
-// }
+  return await client.questions.findFirstOrThrow({
+    where: {
+      type: questionType,
+    },
+    include: {
+      options: true,
+    },
+  });
+}
 
 export default async function Page({
   params,
@@ -27,24 +21,18 @@ export default async function Page({
   params: { type: any };
   children?: React.ReactNode;
 }) {
-  const data = {
-    id: 'Error',
-    type: 'Error',
-    description: 'Error',
-    content: 'Error',
-    options: [],
-  };
+  const data = await fetchData(params);
 
   return (
-    <form method="POST" action={`/api/steps/${data?.type}`}>
+    <form method="POST" action={`/api/steps/${data.type}`}>
       <div className="space-y-4 px-4">
         <h1 className="text-shadow text-4xl font-medium text-muoto-orange">
-          {data?.content}
+          {data.content}
         </h1>
-        <p className="font-medium text-zinc-500">{data?.description}</p>
+        <p className="font-medium text-zinc-500">{data.description}</p>
         <div className="flex flex-row flex-wrap gap-4 ">
-          <input type="hidden" name="questionId" value={data?.id} />
-          {data?.options.map((option: any) => (
+          <input type="hidden" name="questionId" value={data.id} />
+          {data.options.map((option) => (
             <div key={option.id}>
               <Tag option={option} />
             </div>
