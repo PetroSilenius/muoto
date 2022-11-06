40eae39babb0ef1554e5f8acbbfb9cb26cc08a83
Petro Silenius <petro.silenius@gmail.com>
2022-11-06 03:16:45 +0200

    Set viewport size to whole app

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en">
       <head>
         <title>muoto</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
       </head>
       <body className="overflow-y-scroll bg-muoto-blue">
         <div className="grid min-h-screen grid-cols-[min(800px,100%)] place-content-center py-8	">
