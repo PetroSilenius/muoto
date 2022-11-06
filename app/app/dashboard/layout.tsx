1e42577d5f2261f998c8eedf9635a51fb3292b11
Petro Silenius <petro.silenius@gmail.com>
2022-11-06 05:49:57 +0200

    Make dashboard view have a dark background

--- app/app/dashboard/layout.tsx
+++ app/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-4 flex flex-col rounded-md bg-muoto-dark p-4 text-white">
+    <div className="mx-4 flex flex-col rounded-md p-4 text-white">
       <div className="flex justify-between">
         <h1 className="text-2xl">Dashboard</h1>
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css';
+'use client';
 
-export default function RootLayout({ children }: { children: any }) {
+import '@/styles/globals.css';
+import { usePathname } from 'next/navigation';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <head>
@@ -10,7 +18,11 @@ export default function RootLayout({ children }: { children: any }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
       </head>
-      <body className="overflow-y-scroll bg-muoto-blue">
+      <body
+        className={`overflow-y-scroll ${
+          pathname?.includes('dashboard') ? 'bg-muoto-dark' : 'bg-muoto-blue'
+        }`}
+      >
         <div className="grid min-h-screen grid-cols-[min(800px,100%)] place-content-center">
           {children}
         </div>
